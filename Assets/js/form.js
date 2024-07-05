dfecfc16a5586d69084ab4f18d3c534985b0d532
Papito <170674313+PeruvPowWow@users.noreply.github.com>
2024-07-05 12:42:46 -0600

    added content to form js

--- Assets/js/form.js
+++ Assets/js/form.js
@@ -1,7 +1,17 @@
 // TODO: Create a variable that selects the form element
 
 // TODO: Create a function that handles the form submission. Grab the form data and store it in local storage, then redirect to the blog page using the redirectPage function. If the form is submitted with missing data, display an error message to the user.
+const form = document.getElementByID ('blogForm');
 
+const handleFormSubmit = (event) => {
+  event.preventDefault();
+
+  const userName = document.getElementById('username').value;
+  const title = document.getElementById('title').value;
+  const content = document.getElementByID('content').value;
+  const errorMessage = document.getElementByID('errorMessage');
+  
+}
 let redirectURL = '';
 
 const redirectPage = function (url) {
